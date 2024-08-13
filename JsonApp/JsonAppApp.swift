8241a9fca3eacbc139720e8e241bcbf0f6f551b6
Jozek Andrzej Hajduk Sánchez <hajduksanchez.dev@gmail.com>
2024-08-13 14:08:30 -0500

    Initial Commit

---
+++ JsonApp/JsonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonAppApp.swift
+//  JsonApp
+//
+//  Created by Jozek Hajduk on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct JsonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
