4050d100d766ecfe6014955e9f6c0205be170fa5
Jozek Andrzej Hajduk Sánchez <hajduksanchez.dev@gmail.com>
2024-08-13 14:58:09 -0500

    add auth Model and ViewModel

---
+++ JsonApp/Model/Auth.swift
@@ -0,0 +1,10 @@
+//
+//  Auth.swift
+//  JsonApp
+//
+//  Created by Jozek Hajduk on 13/08/24.
+//
+
+struct Auth: Codable {
+    var token: String
+}
---
+++ JsonApp/ViewModel/AuthViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  AuthViewModel.swift
+//  JsonApp
+//
+//  Created by Jozek Hajduk on 13/08/24.
+//
+
+import Foundation
+
+class AuthViewModel: ObservableObject {
+    @Published var authenticated = false
+    
+    func login(email: String, password: String) {
+        guard let url = URL(string: "https://reqres.in/api/login") else { return }
+        let parameters = ["email": email, "password": password]
+        let body = try! JSONSerialization.data(withJSONObject: parameters)
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = body
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else { return }
+            do {
+                let newData = try JSONDecoder().decode(Auth.self, from: data)
+                if !newData.token.isEmpty {
+                    DispatchQueue.main.async {
+                        self.authenticated = true
+                    }
+                }
+            } catch let error as NSError {
+                print("Error", error.localizedDescription)
+                DispatchQueue.main.async {
+                    self.authenticated = false
+                }
+            }
+        }.resume()
+    }
+}
