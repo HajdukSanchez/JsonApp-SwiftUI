410259f94b768dce3cb3d4318433f06690ba2f9f
Jozek Andrzej Hajduk Sánchez <hajduksanchez.dev@gmail.com>
2024-08-16 17:50:13 -0500

    Keep user session open and close session

--- JsonApp/View/HomeView.swift
+++ JsonApp/View/HomeView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject private var authViewModel: AuthViewModel
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            Text("Welcome")
+                .navigationBarTitle("Home")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button {
+                            authViewModel.logOut()
+                        } label: {
+                            Text("Exit")
+                        }
+                    }
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            // TODO: add action
+                        } label: {
+                            Text("Next")
+                        }
+                    }
+                }
+        }
     }
 }
 
--- JsonApp/ViewModel/AuthViewModel.swift
+++ JsonApp/ViewModel/AuthViewModel.swift
@@ -10,6 +10,13 @@ import Foundation
 class AuthViewModel: ObservableObject {
     @Published var authenticated = false
     @Published var error = false
+    private let authKey = "authKey"
+    
+    init() {
+        if let session = UserDefaults.standard.object(forKey: authKey) as? Bool {
+            authenticated = session
+        }
+    }
     
     func login(email: String, password: String) {
         guard let url = URL(string: "https://reqres.in/api/login") else { return }
@@ -29,6 +36,8 @@ class AuthViewModel: ObservableObject {
                     DispatchQueue.main.async {
                         self.authenticated = true
                         self.error = false
+                        // Save session in user default
+                        UserDefaults.standard.setValue(true, forKey: self.authKey)
                     }
                 }
             } catch let error as NSError {
@@ -39,5 +48,10 @@ class AuthViewModel: ObservableObject {
                 }
             }
         }.resume()
+    }
+    
+    func logOut() {
+        UserDefaults.standard.removeObject(forKey: authKey)
+        authenticated = false
     }
 }
